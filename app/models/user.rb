68bd114f1d60bbfed77ac95964b8a163bee8b20d
shmizuyui <yuihana760@gmail.com>
2020-09-15 17:16:30 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 validates :nickname, presence: true
-
+validates :last_name, presence: true
+validates :first_name, presence: true
+validates :last_name_kana, presence: true
+validates :first_name_kana, presence: true
+validates :birth_date, presence: true
 end
