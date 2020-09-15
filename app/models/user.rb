5aa5a7313edb00afe530384b21018d708955e0d6
shmizuyui <yuihana760@gmail.com>
2020-09-15 19:14:34 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 validates :nickname, presence: true
-validates :last_name, presence: true
-validates :first_name, presence: true
-validates :last_name_kana, presence: true
-validates :first_name_kana, presence: true
+
+with_options presence: true do
+validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+end
 validates :birth_date, presence: true
 end
