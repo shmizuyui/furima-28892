65bd99b8b312a76299a02c3e876643fb602bf292
shmizuyui <yuihana760@gmail.com>
2020-09-17 15:19:04 +0900

    コンマ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
     validates :nickname
     validates :birth_date
   end
-  with_options presence: true format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."} do
     validates :last_name
     validates :first_name
   end
-  with_options presence: true format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
     validates :last_name_kana
     validates :first_name_kana
   end
