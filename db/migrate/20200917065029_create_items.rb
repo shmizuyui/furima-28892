34577c38c566720877cf64dd311f7912521a304c
shmizuyui <yuihana760@gmail.com>
2020-09-17 16:06:20 +0900

    itemsテーブルの作成

---
+++ db/migrate/20200917065029_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user, null: false, foreign_key: true 
+      t.text :description,   null: false 
+      t.integer :category,   null: false 
+      t.integer :condition,  null: false 
+      t.integer :postage_payer,  null: false
+      t.integer :prefecture,  null: false
+      t.integer :handing_time,  null: false
+      t.integer :price,  null: false
+      t.timestamps
+    end
+  end
+end
