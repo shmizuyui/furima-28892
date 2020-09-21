b81d1fc61390408cff872385688d1239e90c4000
shmizuyui <yuihana760@gmail.com>
2020-09-21 14:20:48 +0900

    出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+    redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :text, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handing_time_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_index
--- app/models/handing_time.rb
+++ app/models/handing_time.rb
@@ -1,4 +1,4 @@
-class Handingtime < ActiveHash::Base
+class HandingTime < ActiveHash::Base
  self.data = [
    { id:1, name: '---' },
    { id:2, name: '1日〜2日で発送' },
