5fc700820cc555fd146a5db754d0aae8fe533deb
shmizuyui <yuihana760@gmail.com>
2020-09-21 19:27:38 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :handing_time
   has_one_attached :image
+
+  with_options presence: true do
+    validates :text
+    validates :description
+    validates :price
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category
+    validates :condition
+    validates :postage_payer
+    validates :prefecture
+    validates :handing_time
+  end
 end
