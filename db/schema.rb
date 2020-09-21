abc4b4b6f103a018e9f6b930492f6028b42155ef
shmizuyui <yuihana760@gmail.com>
2020-09-21 14:13:01 +0900

    textカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_09_18_063720) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.text "text", null: false
     t.text "description", null: false
     t.integer "category_id", null: false
     t.integer "condition_id", null: false
