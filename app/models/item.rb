c71401d3e34ba14db8a3bbd77802b10aa66fb333
shmizuyui <yuihana760@gmail.com>
2020-10-06 16:17:23 +0900

    アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  has_one :purchase
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage_payer
