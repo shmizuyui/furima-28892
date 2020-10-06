f42533f9bb6fa3509aeaea9ee1fbd1ab333f5f84
shmizuyui <yuihana760@gmail.com>
2020-10-06 22:42:43 +0900

    アソシエーションとバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :order
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture
+    validates :city
+    validates :block
+    validates :building
+    validates :phone_number
+  end
 end
