14c771edd6d1e120615fe4b3eefddfb86ab78982
shmizuyui <yuihana760@gmail.com>
2020-10-06 21:19:57 +0900

    purchase削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_10_06_064911) do
+=======
+ActiveRecord::Schema.define(version: 2020_09_18_063720) do
+>>>>>>> parent of b917ca2... purchaseモデルの生成
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -61,15 +65,6 @@ ActiveRecord::Schema.define(version: 2020_10_06_064911) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_purchases_on_item_id"
-    t.index ["user_id"], name: "index_purchases_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -91,6 +86,4 @@ ActiveRecord::Schema.define(version: 2020_10_06_064911) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "purchases"
   add_foreign_key "items", "users"
-  add_foreign_key "purchases", "items"
-  add_foreign_key "purchases", "users"
 end
