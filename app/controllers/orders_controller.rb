8fb9dc8012691c38d98099111f1eb98324f4ac9f
shmizuyui <yuihana760@gmail.com>
2020-10-07 19:27:37 +0900

    購入画面へ遷移

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,18 @@
+class OrdersController < ApplicationController
+  before_action :set_item
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :order
   with_options presence: true do
-    validates :post_code
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture
     validates :city
     validates :block
     validates :building
-    validates :phone_number
+    validates :phone_number, format: { with: /\A[0-9]+\z/}
   end
 end
